d485631a4b70daef7550a4c343f477f8aecaea6f
JustTams <sarg.tier1@gmail.com>
2019-06-30 02:44:07 -0500

    mobile style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,7 +8,6 @@ const Header = () => {
     </header>
   )
 }
-
 const StyledTitle = styled.h1`
   border-radius: 30px;
   font-size: 2em;
@@ -17,9 +16,5 @@ const StyledTitle = styled.h1`
   color: azure;
   background-color: #85c1e9;
   box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.2), 0 2px 10px 0 rgba(0, 0, 0, 0.19);
-
-
 `
-
-
 export default Header
