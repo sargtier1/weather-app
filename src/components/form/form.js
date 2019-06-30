8fcf50e20eca8a476f41c9436b0b66de55c6b907
JustTams <sarg.tier1@gmail.com>
2019-06-30 00:35:34 -0500

    added new form styles

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
 
-const Form = props => (
-  <div id="form-container">
-    <form onSubmit={props.getWeather}>
-      <label>City</label>
+import Button from '@material-ui/core/Button'
 
-      <input type="text" name="city" placeholder="Somewhere" />
-      <br />
-      <label>Country (Abbr.)</label>
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
+  input: {
+    display: 'none'
+  }
+}))
 
-      <input type="text" name="country" placeholder="US, UK, GR" />
-      <br />
-      <button>Get Weather</button>
-    </form>
-  </div>
-);
+const Form = props => {
+  var classes = useStyles()
+  return (
+    <div id='form-container'>
+      <form onSubmit={props.getWeather}>
+        {/* <label>City</label>
 
-export default Form;
+        <input type='text' name='city' placeholder='Somewhere' />
+        <br />
+        <label>Country (Abbr.)</label>
+
+        <input type='text' name='country' placeholder='US, UK, GR' />
+        <br /> */}
+
+        <TextField
+          id='standard-password-input'
+          label='city'
+          name='city'
+          className={classes.textField}
+          type='text'
+       
+          margin='normal'
+        />
+        <br />
+        <TextField
+          id='standard-password-input'
+          label='country'
+          name='country'
+          className={classes.textField}
+          type='text'
+      
+          margin='normal'
+        />
+        <br />
+        {/* <Button variant='contained' className={classes.button}>
+          Default
+        </Button> */}
+              <button>Get Weather</button>
+
+      </form>
+    </div>
+  )
+}
+export default Form
